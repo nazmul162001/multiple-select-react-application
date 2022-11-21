f9aedfa52cf5b9aa878ec3833eb2f0f8e0866592
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2022-11-22 00:18:17 +0600

    added style to select form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css'
+import Select from 'react-select'
 
 function App() {
+  const options = [
+    { value: 'Orange', label: 'Orange' },
+    { value: 'Blue', label: 'Blue' },
+    { value: 'Yellow', label: 'Yellow' },
+    { value: 'Ocean', label: 'Ocean' },
+    { value: 'Purple', label: 'Purple' },
+    { value: 'Red', label: 'Red' },
+    { value: 'Green', label: 'Green' },
+    { value: 'Forest', label: 'Forest' },
+    { value: 'Slate', label: 'Slate' },
+    { value: 'Silver', label: 'Silver' },
+  ]
+
   return (
     <div className='select-select'>
-        <h1>select your option</h1>
+      <h1>select your option</h1>
+      <Select className='select-form' options={options} />
     </div>
   )
 }
